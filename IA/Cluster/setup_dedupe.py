56c92722f8e57f161261d652ee9140bbc870a5ee
Tábatha Fróes <tabathafroes@gmail.com>
2021-11-28 16:15:39 -0300

    alterando setup_dedupe.py

--- IA/Cluster/setup_dedupe.py
+++ IA/Cluster/setup_dedupe.py
@@ -3,7 +3,7 @@ import os
 
 def configure_dedupe():
     path = os.getcwd() + '\\lib_dedupe\\convenience.py'
-    dest = os.getenv("LIB_PYTHON") + '\\dedupe\\convenience.py'
-    shutil.copy(path, dest) 
+    dest = os.getenv("LIB_PYTHON")+"\\dedupe\\convenience.py"
+    shutil.copy(str(path), str(dest)) 
 
 configure_dedupe()
